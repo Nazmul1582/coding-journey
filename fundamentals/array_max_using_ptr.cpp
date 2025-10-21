905ae376413ebc72b92161cd89c6800b445203d1
Nazmul1582 <nazmulhasan1582@gmail.com>
2025-10-22 00:35:43 +0600

    array length using sizeof

--- fundamentals/array_max_using_ptr.cpp
+++ fundamentals/array_max_using_ptr.cpp
@@ -16,7 +16,10 @@ int findMax(int *ptr, int size)
 int main()
 {
     int arr[5] = {23, 45, 65, 34, 12};
-    int result = findMax(&arr[0], 5);
+    int size = sizeof(arr);                    // int -> 4,     arr[5] -> 20
+    int length = sizeof(arr) / sizeof(arr[0]); // 20 / 4 = 5
+    cout << "sizeof: " << size << " and length: " << length << endl;
+    int result = findMax(&arr[0], length);
     cout << "Result: " << result;
     return 0;
 }
