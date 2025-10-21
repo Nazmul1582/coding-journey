7be504ad8fbcee3ba9df24cf979473ab80513eea
Nazmul1582 <nazmulhasan1582@gmail.com>
2025-10-21 23:34:27 +0600

    finding array max with negative number

--- fundamentals/array_max.cpp
+++ fundamentals/array_max.cpp
@@ -3,11 +3,27 @@ using namespace std;
 
 // find max from the array
 // array element inside the array must be >= zero
-int arr[5] = {23, 89, 34, 98, 47};
+// int arr[5] = {23, 89, 34, 98, 47};
+// int main()
+// {
+//     int max = 1;
+//     for (int i = 0; i < 5; ++i)
+//     {
+//         if (arr[i] > max)
+//         {
+//             max = arr[i];
+//         }
+//     }
+//     cout << max;
+//     return 0;
+// }
+
+// array element inside the array must be >= -100;
+int arr[5] = {-40, 25, -89, -57, 23};
 int main()
 {
-    int max = 1;
-    for (int i = 0; i < 5; ++i)
+    int max = -100;
+    for (int i = 0; i < 5; i++)
     {
         if (arr[i] > max)
         {
